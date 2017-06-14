d6e26bbd63110b22a8b5d7aff27f32ee9b70763a
Aleksandr Yashchuk <Aleksandr.Yashchuk@redsys.ru>
2017-06-14 11:58:09 +0300

    Сортировка списка Групп тестов при выводе через темплейт.

--- modules/handlers/index-page.go
+++ modules/handlers/index-page.go
@@ -5,14 +5,17 @@ import (
 	"net/http"
 	"../helpers"
 	log "github.com/Sirupsen/logrus"
+	"sort"
 )
 
 var Version string = "1.5"
 
 func ShowIndexPage(context *gin.Context)  {
 
+	var testGroupList []string = helpers.GetTestGroupsList()
 
-	testGroupList := helpers.GetTestGroupsList()
+	// Отсортировать список Групп
+	sort.Strings(testGroupList)
 
 	for idx, group := range testGroupList {
 		log.Infof("%d.%s", idx, group)
