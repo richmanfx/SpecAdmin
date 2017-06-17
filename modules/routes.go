61c2857b86576635a9a1358adfb8fab302ca01d3
Aleksandr Jashhuk <richmanfx@yandex.ru>
2017-06-17 22:49:57 +0300

    Длина полей input увеличена. Начато редактирование сюит.

--- modules/routes.go
+++ modules/routes.go
@@ -16,5 +16,6 @@ func InitRoutes(router *gin.Engine) {
 
 	router.Handle("POST", "/spec-admin/add-suite", handlers.AddSuite)
 	router.Handle("POST", "/spec-admin/del-suite", handlers.DelSuite)
+	router.Handle("POST", "/spec-admin/edit-suite", handlers.EditSuite)
 }
 
