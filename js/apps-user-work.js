7e4c72d2fbebc9ccef6c9cd1d0e9d8b5ecc4750d
Aleksandr Yashchuk <Aleksandr.Yashchuk@redsys.ru>
2017-07-27 16:45:48 +0300

    Модальная форма изменения пароля. Вывод логина на label модальной формы изменения пароля.

--- js/apps-user-work.js
+++ js/apps-user-work.js
@@ -2,9 +2,7 @@
  * Created by Александр Ящук (R5AM, Zoer) on 27.07.2017.
  */
 
-
 /// Подстановка Логина и Полного имени пользователя в поля в модальном окне
-
 // Для кнопки "Удалить"
 $('#deleteUser').on('show.bs.modal', function (event) {
 
@@ -19,7 +17,17 @@ $('#deleteUser').on('show.bs.modal', function (event) {
     var modal = $(this);
 
     // В input-ы вставить
-    modal.find('#id_login').val(deletedLogin)
-    modal.find('#id_full_name').val(deletedFullName)
+    modal.find('#id_login').val(deletedLogin);
+    modal.find('#id_full_name').val(deletedFullName);
 });
 
+
+/// Вывод логина на label модальной формы изменения пароля
+// Для кнопки "Изменить пароль"
+$('#changePassword').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var Login = button.data('login');
+    var modal = $(this);
+    modal.find('#id_login_label').val(Login);
+    document.getElementById('id_login_label').innerHTML = ' ' + Login;
+});
